7176c1daac8f4b7e9c1ec0077cced49f470942bc
gokoreas <gokoreas@gmail.com>
2016-08-03 01:07:06 +0900

    Outdoor test preparations
    Added test_setpoints_traj: Disabled arm/offboard mode services for hand triggering offboard mode
    Added ctrl_traj_test.launch for test outdoors

---
+++ src/test_setpoints_traj.cpp
@@ -0,0 +1,78 @@
+/**
+ * @file offb_node.cpp
+ * @brief offboard example node, written with mavros version 0.14.2, px4 flight
+ * stack and tested in Gazebo SITL
+ */
+
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include "math.h"
+
+double r;
+double theta;
+double count=0.0;
+double wn;
+
+mavros_msgs::State current_state;
+void state_cb(const mavros_msgs::State::ConstPtr& msg){
+    current_state = *msg;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "offb_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+            ("mavros/state", 10, state_cb);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("mavros/setpoint_position/local", 10);
+
+    //the setpoint publishing rate MUST be faster than 2Hz
+    ros::Rate rate(20.0);
+	
+
+	nh.param("pub_setpoints_traj/wn", wn, 1.0);
+	nh.param("pub_setpoints_traj/r", r, 1.0);
+    // wait for FCU connection
+    while(ros::ok() && current_state.connected){
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    geometry_msgs::PoseStamped pose;
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 0;
+    pose.pose.position.z = 2;
+
+    //send a few setpoints before starting
+    for(int i = 100; ros::ok() && i > 0; --i){
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    ros::Time last_request = ros::Time::now();
+
+    while(ros::ok()){
+       
+
+	theta = wn*count*0.05;
+
+    	pose.pose.position.x = r*sin(theta);
+    	pose.pose.position.y = r*cos(theta);
+    	pose.pose.position.z = 2;
+
+	count++;
+
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
+
